417fdd47a4a6bf8d6f209b17ba24ac1fc5c73c9a
TheRogue <36058852+rogue7373@users.noreply.github.com>
2023-09-05 16:30:24 -0600

    Day 8 - Update 5 - 100 Days of Code
    Added support for username for both Windows and MacOS. Working on pulling vpnName and vpnIP.

--- sampleProjects/hostGrabber/main.go
+++ sampleProjects/hostGrabber/main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"os/exec"
 	"runtime"
+	"strings"
 	// "github.com/felixge/netroute/table"
 )
 
+func getVPNInfo() (string, string) {
+	cmd := exec.Command("scutil", "--nc", "list")
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(out)
+	err = cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+	rawOutput := ""
+	for scanner.Scan() {
+		rawOutput += scanner.Text() + "\n"
+	}
+	cmd.Wait()
+
+	vpnName := ""
+	vpnIP := ""
+
+	if strings.Contains(rawOutput, "Connected") {
+		lines := strings.Split(rawOutput, "\n")
+		for _, line := range lines {
+			if strings.Contains(line, "Connected") {
+				vpnName = strings.Split(line, " (Connected by ")[0]
+			}
+			if strings.Contains(line, "ServerAddress : ") {
+				vpnIP = strings.Replace(strings.Split(line, "ServerAddress : ")[1], "Address : ", "", -1)
+			}
+		}
+	}
+	return vpnName, vpnIP
+}
 func getHostInfo() (string, string, string, string) {
 	// Get the Hostname
 	hostname, err := os.Hostname()
@@ -67,16 +103,28 @@ func getHostInfo() (string, string, string, string) {
 	//gateway := defaultRoute.Gateway.String()
 	//subnetMask := net.IP(defaultRoute.Mask).String()
 
+	// Get the VPN information
+
 	return hostname, username, ipAddress, macAddress
 }
 func main() {
 	hostname, username, ipAddress, macAddress := getHostInfo()
+
+	vpnName, vpnIP := getVPNInfo()
+
+	// Print Results
 	var i = fmt.Println
 
 	i("Hostname: ", hostname)
 	i("Username: ", username)
 	i("IP Address: ", ipAddress)
 	i("MAC Address: ", macAddress)
+	if runtime.GOOS == "darwin" {
+		i("VPN Name:", vpnName)
+		i("VPN IP Address:", vpnIP)
+	} else if runtime.GOOS == "windows" {
+		i("VPN IP Address:", vpnIP)
+	}
 	// i("Gateway: ", gateway)
 	// i("Subnet Mask: ", subnetMask)
 }
